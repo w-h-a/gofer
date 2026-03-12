e0bf1653a35de4137b9f63f4c31eac6c089edf93
Wes Anderson <wesley.herb.anderson@gmail.com>
2026-03-12 10:38:36 -0700

    feat: instrument SSE event publisher with OTel spans (#31)

--- internal/client/event_publisher/sse/event_publisher.go
+++ internal/client/event_publisher/sse/event_publisher.go
@@ -7,29 +7,53 @@ import (
 	"github.com/google/uuid"
 	eventpublisher "github.com/w-h-a/gofer/internal/client/event_publisher"
 	"github.com/w-h-a/gofer/internal/domain"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type sseEventPublisher struct {
 	options     eventpublisher.Options
 	mtx         sync.RWMutex
 	subscribers map[uuid.UUID][]chan domain.CapturedRequest
+	tracer      trace.Tracer
 }
 
 func (p *sseEventPublisher) Publish(ctx context.Context, req domain.CapturedRequest) error {
+	ctx, span := p.tracer.Start(ctx, "sseEventPublisher.Publish")
+	defer span.End()
+
 	p.mtx.RLock()
 	defer p.mtx.RUnlock()
 
-	for _, ch := range p.subscribers[req.BinID()] {
+	subs := p.subscribers[req.BinID()]
+	notified := 0
+
+	for _, ch := range subs {
 		select {
 		case ch <- req:
+			notified++
 		default:
 		}
 	}
+
+	span.SetAttributes(
+		attribute.String("bin.id", req.BinID().String()),
+		attribute.Int("subscribers.count", len(subs)),
+		attribute.Int("subscribers.notified", notified),
+	)
 
 	return nil
 }
 
 func (p *sseEventPublisher) Subscribe(ctx context.Context, binID uuid.UUID) (<-chan domain.CapturedRequest, error) {
+	_, span := p.tracer.Start(ctx, "sseEventPublisher.Subscribe")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.String("bin.id", binID.String()),
+	)
+
 	ch := make(chan domain.CapturedRequest, 16)
 
 	p.mtx.Lock()
@@ -40,6 +64,9 @@ func (p *sseEventPublisher) Subscribe(ctx context.Context, binID uuid.UUID) (<-c
 }
 
 func (p *sseEventPublisher) Unsubscribe(ctx context.Context, binID uuid.UUID, ch <-chan domain.CapturedRequest) error {
+	_, span := p.tracer.Start(ctx, "sseEventPublisher.Unsubscribe")
+	defer span.End()
+
 	p.mtx.Lock()
 	defer p.mtx.Unlock()
 
@@ -57,6 +84,11 @@ func (p *sseEventPublisher) Unsubscribe(ctx context.Context, binID uuid.UUID, ch
 		delete(p.subscribers, binID)
 	}
 
+	span.SetAttributes(
+		attribute.String("bin.id", binID.String()),
+		attribute.Int("subscribers.remaining", len(p.subscribers[binID])),
+	)
+
 	return nil
 }
 
@@ -67,6 +99,7 @@ func NewEventPublisher(opts ...eventpublisher.Option) (eventpublisher.EventPubli
 		options:     options,
 		mtx:         sync.RWMutex{},
 		subscribers: map[uuid.UUID][]chan domain.CapturedRequest{},
+		tracer:      otel.Tracer("github.com/w-h-a/gofer/internal/client/event_publisher/sse"),
 	}
 
 	return p, nil
